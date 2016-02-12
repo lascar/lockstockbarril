a3eac2eeacb21db8325291f075afa45e5bb17a8c
lascar <pascal.carrie@gmail.com>
2016-02-12 16:58:06 +0100

    inverse of for supplies

--- app/models/supply.rb
+++ app/models/supply.rb
@@ -1,6 +1,6 @@
 class Supply < ActiveRecord::Base
-  belongs_to :article
-  belongs_to :supplier
+  belongs_to :article, inverse_of: :supplies
+  belongs_to :supplier, inverse_of: :supplies
   validates_uniqueness_of :ref_supplier, scope: :supplier_id
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
 end
