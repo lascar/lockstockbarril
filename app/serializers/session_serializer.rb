0f797d32d458d251fd79c8963688efa665e86f1e
lascar <pascal.carrie@gmail.com>
2016-02-29 15:26:08 +0100

    essai promote

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,14 +1,17 @@
 # session_serializer serialize the session
 class SessionSerializer < ActiveModel::Serializer
 
-  attributes :email, :token_type, :user_id, :access_token
+  attributes :email, :token_type, :user_id, :access_token, :to_promote
 
   def user_id
     object.id
   end
 
   def token_type
-    'aaaa'
+    'Bearer'
   end
-
+ 
+  def to_promote
+    'promote'
+  end
 end
